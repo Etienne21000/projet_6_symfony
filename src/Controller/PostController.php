0f67b8c15f176c8f81fd6a6aae281397f2013619
Etienne Juffard <etiennejuffard@gmail.com>
2021-04-05 18:49:12 +0200

    Create postType form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,7 +4,12 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\PostType;
+use App\Entity\Post;
+use App\Repository\PostRepository;
+
 
 class PostController extends AbstractController
 {
@@ -15,6 +20,26 @@ class PostController extends AbstractController
     {
         return $this->render('base.html.twig', [
             'controller_name' => 'PostController',
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     * @Route("/addPost", name="addPost")
+     */
+    public function addPostView(Request $request){
+        $title = 'Ajouter une nouvelle figure';
+        $sub = 'Ajouter un trick en choisissant sa catégorie';
+
+        $post = new Post();
+
+        $form = $this->createForm(PostType::class, $post);
+
+        return $this->render('main/add_post_view.html.twig', [
+            'title' => $title,
+            'sub' => $sub,
+            'form' => $form->createView(),
         ]);
     }
 }
